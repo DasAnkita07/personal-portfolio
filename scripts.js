c000abc7899e07dd847e6f3a55ee5e1415d80071
DasAnkita07 <aankitaadaas@gmail.com>
2026-06-28 17:34:57 +0530

    Initial portfolio commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const heroTitle = document.querySelector(".hero h1");
+console.log(heroTitle);
+
+// const subtitle = document.querySelector(".hero h2");
+// subtitle.textContent = "Future Full Stack Developer";
